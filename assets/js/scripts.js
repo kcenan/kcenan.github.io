2a976b8b93b256df1d32533e7b670e9317ae4068
Kagan Cenan <kcenan@Kagans-Air.lan>
2023-06-01 11:48:14 +0100

    Portfolio website init

---
+++ assets/js/scripts.js
@@ -0,0 +1,30 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+function read_more_action(dot_id,more_id,btn_id) {
+  var dots = document.getElementById(dot_id);
+  var moreText = document.getElementById(more_id);
+  var btnText = document.getElementById(btn_id);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
